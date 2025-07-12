ce21a83bda6ffd9455d17a59cd1f8ce298fa3f15
gjorgji-ts <74930137+gjorgji-ts@users.noreply.github.com>
2025-07-12 21:23:56 +0200

    fix(index): update comment to clarify modification instructions for entrypoint

--- dist/index.js
+++ dist/index.js
@@ -30767,6 +30767,7 @@ async function run() {
 /**
  * The entrypoint for the action. This file simply imports and runs the action's
  * main logic.
+ * Do not modify this file directly; instead, make changes in `main.js`.
  */
 /* istanbul ignore next */
 run();
